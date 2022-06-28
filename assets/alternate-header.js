9086a2e70ee9ecfbbbca2951ff69ae850d357ffc
Aleksandr Sumaneev <caho@mail.ua>
2022-06-28 23:40:31 +0300

    feat: shopify theme section for alternate-header

--- assets/alternate-header.js
+++ assets/alternate-header.js
@@ -1,12 +1,46 @@
-document.addEventListener('cart:added', (CustomEvent) => {
-  const newHeader = CustomEvent.detail.header;
-  const selectorAmountCart = '#product-amount-cart';
+function CartAmount() {
+  this.cartListener = () => {
 
-  const newCartAmount = new DOMParser()
-  .parseFromString(newHeader, "text/html")
-  .querySelector(selectorAmountCart)
-  .innerHTML;
+    document.addEventListener('cart:added', (CustomEvent) => {
+      const newHeader = CustomEvent.detail.header;
+      const selectorAmountCart = '#product-amount-cart';
+    
+      const newCartAmount = new DOMParser()
+      .parseFromString(newHeader, "text/html")
+      .querySelector(selectorAmountCart)
+      .innerHTML;
+    
+      document.querySelector(selectorAmountCart).innerHTML = newCartAmount;
+      
+    });
+  }
 
-  document.querySelector(selectorAmountCart).innerHTML = newCartAmount;
+  this.removeCartListener = () => {
+    document.removeEventListener('cart:added');
+  }
+
+}
+
+
+Shopify.theme.sections.register('alternate-header', {
+
+  cartAmount: null,
   
-})
+  onLoad: function() {
+    // Do something when a section instance is loaded
+    this.cartAmount = new CartAmount();
+    this.cartAmount.cartListener();
+    
+  },
+
+  
+  onUnload: function() {
+    // Do something when a section instance is unloaded
+    if (this.cartAmount) {
+      this.cartAmount.removeCartListener();
+    }
+
+  },
+
+  
+});
