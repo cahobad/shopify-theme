04ff3985913626aa688aef8d9ca53f67d03de91e
Aleksandr Sumaneev <36692205+cahobad@users.noreply.github.com>
2022-06-30 21:28:49 +0000

    Webpack (#3)
    * feat: download webpack for my shopify theme
    
    * feat: add js and css files for alternate product
    
    * fix: files for webpack
    
    * feat: add theme product form
    
    Co-authored-by: Aleksandr Sumaneev <caho@mail.ua>

---
+++ src/scripts/index.js
@@ -0,0 +1,6 @@
+import {load} from '@shopify/theme-sections';
+
+import './sections/alternate-main-product';
+import './sections/alternate-header';
+
+document.addEventListener('DOMContentLoaded', () => load('*'));
