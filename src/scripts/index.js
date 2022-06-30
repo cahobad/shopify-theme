f74ba6e4d6c8fbadc4f56404ab156cf3bf4682ce
Aleksandr Sumaneev <caho@mail.ua>
2022-06-30 16:57:57 +0300

    feat: add js and css files for alternate product

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import {load} from '@shopify/theme-sections';
 
 import './sections/alternate-main-product';
+import './sections/alternate-header';
 
 document.addEventListener('DOMContentLoaded', () => load('*'));
